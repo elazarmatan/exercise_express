5bf9fec2979cee9ddbde3e97eec411d9fa99f258
elazar matan <elazarmata8@gmail.com>
2025-07-08 16:56:17 +0300

    This commit introduces a logger middleware, GET endpoints for greeting with a name parameter and testing functionality, and a POST endpoint for future actions.
    Specifically:
    
    -   **Adds a `logger` middleware:** This middleware logs the request method and URL to the console for each incoming request.
    -   **Introduces a `/greet/:name` GET endpoint:** This endpoint responds with a JSON message indicating that the request was received from the specified name.
    -   **Introduces a `/test` GET endpoint:** This endpoint makes an internal `fetch` request to `/greet/elazar` and checks if the response message is "got from elazar".  It then responds with "ok" or "not ok" accordingly.
    -   **Adds a basic `/action` POST endpoint:** This endpoint currently does nothing and is intended for future implementation.

--- index.js
+++ index.js
@@ -4,11 +4,44 @@ const PORT = 2027
 
 const server = expres()
 
-// new Date().toLocaleTimeString()
+function logger(req,res,next){
+    console.log(req.method,req.url)
+    next()
+}
+
+server.use(logger)
+//GETS==============================================================================
+server.get('/greet/:name',(req,res)=>{
+    res.json({"msg":`got from ${req.params.name}`})
+})
+
+server.get('/test',(req,res) =>{
+    fetch('http://localhost:2027/greet/elazar')
+    .then(response => response.json())
+    .then(data => {
+        if(data.msg === 'got from elazar'){
+            res.end('ok')
+        }
+        else{
+            res.end('not ok')
+        }
+    })
+})
+
+
+
 server.get('/greet',(req,res)=>{
     res.json({
             "msg":"hi from get endpoint",
            "time": new Date().toLocaleTimeString()})
+})
+
+
+
+//POSTS-------------------------------------------------------------------------------------------
+
+server.post('/action',(req,res)=>{
+    
 })
 
 server.listen(PORT,()=>console.log(`server listening on port ${PORT}`))
